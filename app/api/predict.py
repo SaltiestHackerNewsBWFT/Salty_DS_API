b8800c642a9a7a22e9ed3d73ae1adea079068336
Ava Wingfield <bwitter770@gmail.com>
2020-07-27 19:12:59 -0400

    got basic API running with small changes

--- app/api/predict.py
+++ app/api/predict.py
@@ -2,28 +2,31 @@ import logging
 import random
 
 from fastapi import APIRouter
+
 import pandas as pd
+
 from pydantic import BaseModel, Field, validator
 
 log = logging.getLogger(__name__)
+
 router = APIRouter()
 
 
 class Item(BaseModel):
     """Use this data model to parse the request body JSON."""
 
-    x1: float = Field(..., example=3.14)
-    x2: int = Field(..., example=-42)
-    x3: str = Field(..., example='banjo')
+    user : str = Field(..., example='banjo')
+    id : int = Field(..., example=-42)
+    text : str = Field(..., example='kazooie')
 
     def to_df(self):
         """Convert pydantic object to pandas dataframe with 1 row."""
         return pd.DataFrame([dict(self)])
 
-    @validator('x1')
+    @validator('id')
     def x1_must_be_positive(cls, value):
-        """Validate that x1 is a positive number."""
-        assert value > 0, f'x1 == {value}, must be > 0'
+        """Validate that id is a positive number."""
+        assert value > 0, f'id == {value}, must be > 0'
         return value
 
 
@@ -32,7 +35,7 @@ async def predict(item: Item):
     """Make random baseline predictions for classification problem."""
     X_new = item.to_df()
     log.info(X_new)
-    y_pred = random.choice([True, False])
+    y_pred = random.choice(["Salty", "Not Salty"])
     y_pred_proba = random.random() / 2 + 0.5
     return {
         'prediction': y_pred,
